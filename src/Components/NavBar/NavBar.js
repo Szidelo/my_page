2c405fc135ca816b7225c2e9c6901801b2655501
Szidelo <claudiuszidelo@yahoo.ro>
2023-11-26 15:47:42 +0200

    photos-corrected

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
 					<li key={index}>
 						<Link onClick={() => setLocation(link.path)} to={link.path} className="link-item d-flex justify-content-start align-items-center gap-2 py-3">
 						<div className={location === link.path ? 'icon-wrapper active' : 'icon-wrapper'}>{link.icon}</div>
-							<p className={location === link.path ? 'body active-para m-1' : 'body m-0'}>{link.text}</p>
+							<p className={location === link.path ? 'body active-para m-0' : 'body m-0'}>{link.text}</p>
 						</Link>
 					</li>
 				))}
