a1559c41693b3e85befc602111945355675884f8
Szidelo <claudiuszidelo@yahoo.ro>
2024-01-02 16:44:17 +0200

    cv updated

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,6 +1,6 @@
 import "./Banner.css";
 import { Link } from "react-router-dom";
-import CV from "../../assets/CV_Szidelo_Claudiu.pdf";
+import CV from '../../assets/CV_ClaudiuSzidelo.pdf'
 
 const Banner = () => {
 	return (
