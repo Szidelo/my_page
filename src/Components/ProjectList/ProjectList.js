4c595bf651280f88d4fd67ab04d575e7d3ee9577
Szidelo <claudiuszidelo@yahoo.ro>
2025-06-22 11:12:21 +0300

    added Odin Restaurant project with image and updated project list sorting

--- src/Components/ProjectList/ProjectList.js
+++ src/Components/ProjectList/ProjectList.js
@@ -3,14 +3,18 @@ import ProjectCard from "../ProjectCard/ProjectCard";
 import data from "../../utils/data.json";
 
 const ProjectList = () => {
+	const fixedOrderIds = ["project9", "project6", "project7", "project3"];
+
+	const fixedProjects = data.projects.filter((project) => fixedOrderIds.includes(project.id));
+
+	const randomProjects = data.projects.filter((project) => !fixedOrderIds.includes(project.id)).sort(() => Math.random() - 0.5); // amestecă restul
+
+	const sortedProjects = [...fixedProjects, ...randomProjects];
+
 	return (
 		<div className="project-list d-flex flex-column gap-5 py-5 mb-5">
-			
-			{data.projects.map((project) => (
-				<ProjectCard
-					key={project.id}
-					data={project}
-				/>
+			{sortedProjects.map((project) => (
+				<ProjectCard key={project.id} data={project} />
 			))}
 		</div>
 	);
